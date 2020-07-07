cfa30fcc5d5ad8208e472dac5203d573e6ad6f21
tdifiori <tdifiori77@gmail.com>
2020-07-07 16:55:42 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,9 @@ import { FormBuilder, FormGroup, Validators, FormControl, ValidatorFn} from '@an
       <hr>
         <form  [formGroup]="reactiveForm" (ngSubmit)="submitForm()" >
           <div class="form-group">
+
             <app-section *ngFor="let section of formConfig.sections" [sectionForm]="section" [group]="reactiveForm" ></app-section>
+            
               <button [disabled]="reactiveForm.invalid" type="submit"  class="btn btn-primary">Invia</button> 
                 <b>  form status:</b> {{ reactiveForm.status }}
                   <hr>
