89b9418426c410a09392d4051a3c5e679108ab90
tdifiori <tdifiori77@gmail.com>
2020-07-22 01:43:19 +0200

    2 files modified

--- src/app/Shared/control/control.component.ts
+++ src/app/Shared/control/control.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { FormGroup } from "@angular/forms";
-import { components } from "./hash-map-component";
+//import { components } from "./hash-map-component";
 import { inputConf } from '../../interface/control';
 
 
 @Component({
   selector: "app-control",
   template: `
-  <ng-container *ngFor="let field of config;" 
+<!--    <ng-container *ngFor="let field of config.field" 
      dynamicControl 
      [config]="field"
      [group]="group"
   >
-  </ng-container> 
+  </ng-container> -->
+
+   <div class="col" *ngFor="let field of config.field">
+dfadfasdf
+  </div>
   `,
   styleUrls: ["./control.component.css"]
 })
@@ -20,9 +24,11 @@ export class ControlComponent implements OnInit {
   @Input('field') config: any;
   @Input('group') group: FormGroup;
 
-  constructor() {}
+  constructor() {
+    
+  }
 
   ngOnInit() {
-    console.log("mi torna questo: ");
+    console.log("ddddddd");
   }
 }
